477c9ad3ab87b3b549f606393af04e096cfa97bc
Caiomerces <caio.merces.dev@gmail.com>
2023-05-28 02:30:03 -0300

    movendo os arquivos para pasta view

--- conf/db/database.js
+++ conf/db/database.js
@@ -4,7 +4,7 @@ var connection = mysql.createConnection({
   host: "localhost",
   database: "winterrecifey_database",
   user: "root",
-  password: "123456",
+  password: "@CaioRogerio1",
 });
 
 module.exports = connection;
--- index.js
+++ index.js
@@ -4,8 +4,17 @@ var app = express();
 
 app.get("/", function (req, res) {
   console.log("pagina do index");
+  let sql = "SELECT * FROM tb_cliente";
+  connection.query(sql, function (err, result) {
+    if (err) throw err;
+    res.send(result);
+  });
 });
 
 app.listen(3000, function () {
   console.log("Servido rodando na porta 3000");
+  connection.connect(function (err) {
+    if (err) throw err;
+    console.log("dataBase connected");
+  });
 });
