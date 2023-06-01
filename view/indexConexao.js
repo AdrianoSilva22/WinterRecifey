2f09b5ff983487590f015eaeff63901bbb9839fb
AdrianoSilva22 <adrianodesilva22@gmail.com>
2023-06-01 17:19:35 -0300

    Criando formulário

--- view/indexConexao.js
+++ view/indexConexao.js
@@ -1,18 +1,63 @@
-const selectElement = document.getElementById('meuSelect')
+// const selectElement = document.getElementById('meuSelect')
 
-const url = "http://localhost:3000/servico"
-function getServico(){
-fetch(url)
-.then(response => response.json())
-.then(data =>{
-    data.forEach(optionData => {
-      const option = document.createElement('option')
-      option.value = optionData.cod_servico
-      option.textContent = optionData.ocupacao
-      selectElement.appendChild(option)
-    });
-})
-.catch( e => console.error(e))
+// const url = "http://localhost:3000/servico"
+// function getServico() {
+//   fetch(url)
+//     .then(response => response.json())
+//     .then(data => {
+//       data.forEach(optionData => {
+//         const option = document.createElement('option')
+//         option.value = optionData.cod_servico
+//         option.textContent = optionData.ocupacao
+//         selectElement.appendChild(option)
+//       });
+//     })
+//     .catch(e => console.error(e))
+// }
+
+// getServico()
+
+
+
+
+clienteObjeto = {}
+function cliente(callback) {
+  
+  document.getElementById("myForm").addEventListener("submit", (event) => {
+    event.preventDefault(); // Previne o comportamento padrão de envio do formulário
+
+    clienteObjeto = {
+      cpf: document.getElementById("cpf").value,
+      telefone: document.getElementById("telefone").value,
+      email: document.getElementById("email").value,
+      nome: document.getElementById("nome").value,
+    }
+
+    return callback(clienteObjeto)
+  });
+
 }
 
-getServico()
+cliente((objeto) => {
+  console.log(objeto);
+});
+
+
+
+
+// const urlPost = "http://localhost:3000/cadastrar"
+
+// function postCadastrar() {
+//   fetch(urlPost, {
+//     cpf: "",
+//     telefone: "",
+//     email: "",
+//     nome: "",
+//     cod_servico_cliente: "",
+
+//   })
+//     .then(response => response.json())
+
+// }
+
+// postCadastrar()
